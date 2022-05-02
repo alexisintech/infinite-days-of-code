a001ab6fbbdae629e3c0175c502e3de972dec62c
Alexis Aguilar <alexmerona@gmail.com>
2022-05-02 19:49:43 -0400

    05022022

--- thejsway-chapter9-characterinventory.js
+++ thejsway-chapter9-characterinventory.js
@@ -66,3 +66,21 @@ class Character {
     }
   }
 
+// Call the characters and actions
+  const aurora = new Character("Aurora", 150, 25);
+  const glacius = new Character("Glacius", 130, 30);
+  
+  console.log("Welcome to the adventure! Here are our heroes:");
+  console.log(aurora.describe());
+  console.log(glacius.describe());
+  
+  const monster = new Character("Spike", 40, 20);
+  console.log("A wild monster has appeared: it's named " + monster.name);
+  
+  monster.attack(aurora);
+  monster.attack(glacius);
+  aurora.attack(monster);
+  glacius.attack(monster);
+  
+  console.log(aurora.describe());
+  console.log(glacius.describe());
