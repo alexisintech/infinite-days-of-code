df6e93d508af1f19dad906d51c4c4e48f6fd6612
Alexis Aguilar <alexmerona@gmail.com>
2022-05-02 19:17:25 -0400

    05022022

--- thejsway-chapter9-dogs.js
+++ thejsway-chapter9-dogs.js
@@ -32,10 +32,18 @@ class Dog {
         this.size = size;
     }
     bark(){
-        console.log('Woof woof!');
+        if (this.size > 60) {
+            return 'Grrr grrr'
+        } else {return 'Woof woof!'}
     }
 }
 
-// ~*~*~*~*~*~* Leon's Solution
+const fang = new Dog("Fang", "boarhound", 75);
+console.log(`${fang.name} is a ${fang.species} dog measuring ${fang.size}`);
+console.log(`Look, a cat! ${fang.name} barks: ${fang.bark()}`);
+
+const snowy = new Dog("Snowy", "terrier", 22);
+console.log(`${snowy.name} is a ${snowy.species} dog measuring ${snowy.size}`);
+console.log(`Look, a cat! ${snowy.name} barks: ${snowy.bark()}`);
 
 
